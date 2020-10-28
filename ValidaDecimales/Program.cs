79b56170353b006cf4a2748f3df1783bc8aa7f00
Haeretik <vera10mx@hotmail.com>
2020-10-28 04:22:48 -0600

    Agregar archivos de proyecto.

---
+++ ValidaDecimales/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidaDecimales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<double>() { 8.32, 433.000, 2.232, 2.0, 54.00, 1.00 };
+            int count = 0;
+            foreach (double Value in list)
+            {
+                count++;
+                String.Format("{0:0.##}", Value);
+                Console.WriteLine($"Element{count }:{Value}");
+            }
+        }
+    }
+}
